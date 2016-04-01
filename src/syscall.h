5b56de4d9f71bb7ae3ab21866257c170786284ce
Kyle Koser <kkoser@nd.edu>
2016-03-31 22:10:07 -0400

    Add Window System Calls
    This adds a preliminary set of window system calls, excluding those
    involving double type parameters (arc drawing). All drawing is done
    in the current process' window. If there is no window for the calling
    process, then no draw calls will occur.

--- src/syscall.h
+++ src/syscall.h
@@ -8,10 +8,19 @@ See the file LICENSE for details.
 #define SYSCALL_H
 
 #include "kerneltypes.h"
+#include "graphics.h"
 
 #define SYSCALL_exit     1
 #define SYSCALL_testcall 2
 #define SYSCALL_yield    3
+
+#define SYSCALL_window_create 200
+#define SYSCALL_window_set_border_color 201
+#define SYSCALL_window_draw_line 202
+#define SYSCALL_window_draw_arc 203
+#define SYSCALL_window_draw_circle 204
+#define SYSCALL_window_draw_char 205
+#define SYSCALL_window_draw_string 206
 
 uint32_t syscall(uint32_t n, uint32_t a, uint32_t b, uint32_t c, uint32_t d,
                  uint32_t e);
@@ -27,5 +36,30 @@ static inline int32_t testcall(int x) {
 static inline int32_t yield() {
     return syscall(SYSCALL_yield, 0, 0, 0, 0, 0);
 }
+
+// Window System Calls
+static inline int32_t create_window(int x, int y, int width, int height) {
+	return syscall(SYSCALL_window_create, x, y, width, height, 0);
+}
+
+static inline int32_t set_border_color(const struct graphics_color *border_color) {
+	return syscall(SYSCALL_window_set_border_color, (int)border_color, 0, 0, 0, 0);
+}
+
+static inline int32_t draw_line(int x1, int y1, int x2, int y2, const struct graphics_color *color) {
+	return syscall(SYSCALL_window_draw_line, x1, y1, x2, y2, (int)color);
+}
+
+static inline int32_t draw_char(int x, int y, char c, const struct graphics_color *fgcolor, const struct graphics_color *bgcolor) {
+	return syscall(SYSCALL_window_draw_char, x, y, c, (int)fgcolor, (int)bgcolor);
+}
+
+static inline int32_t draw_string(int x, int y, const char *str, const struct graphics_color *fgcolor,
+	const struct graphics_color *bgcolor) {
+	return syscall(SYSCALL_window_draw_string, x, y, (int)str, (int)fgcolor, (int)bgcolor);
+}
+
+
+
 
 #endif
